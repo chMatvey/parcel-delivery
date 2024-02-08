00af94bec2cd5058827415e225fde90fcb27e4e3
Matvey Chudakov <matvey159@gmail.com>
2024-02-09 01:03:45 +0300

    Fixed delivery database schema

--- delivery/src/main/resources/db/migration/V1__schema.sql
+++ delivery/src/main/resources/db/migration/V1__schema.sql
@@ -6,12 +6,12 @@ create table deliveries
 
     status           varchar(20)        not null,
     completed        boolean            not null default false,
-    cancelled        boolean            not null default false,
+    canceled         boolean            not null default false,
 
     source_address   varchar(255)       not null,
     delivery_address varchar(255)       not null,
 
-    delivery_date    date               not null,
+    delivery_date    timestamp          not null,
 
     version          integer            not null,
     created_at       timestamp          not null default now(),
@@ -19,6 +19,6 @@ create table deliveries
 );
 
 create index deliveries_courier_id_order_id_idx on deliveries (courier_id, order_id)
-    where completed = false and cancelled = false;
+    where completed = false and canceled = false;
 
 create index deliveries_order_idx on deliveries (order_id);
