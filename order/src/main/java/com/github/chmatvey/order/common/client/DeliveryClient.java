6f58071a01056af7e2b693427dd4da2ef5df8b0c
Matvey Chudakov <matvey159@gmail.com>
2024-02-10 00:12:25 +0300

    Rabbitmq

--- order/src/main/java/com/github/chmatvey/order/common/client/DeliveryClient.java
+++ order/src/main/java/com/github/chmatvey/order/common/client/DeliveryClient.java
@@ -3,7 +3,6 @@ package com.github.chmatvey.order.common.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "DeliveryClient", url = "${integration.delivery.url}")
 public interface DeliveryClient {
